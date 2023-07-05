7800e313a04c56daeeec1e22ba3af4f7744496e6
sparky <benro2211@gmail.com>
2023-07-05 13:43:09 -0600

    refactor

--- inverterTemp_CAN.py
+++ inverterTemp_CAN.py
@@ -9,11 +9,13 @@ import os
 # this is the method used by hwi/danfoss dyno app
 # was using load_file method instead of add_file
 # this adds all dbc's in a folder "./dbc"
-db = cantools.database.Database()
-for file in os.listdir('./dbc'):
-    #print(file)
-    if file.endswith('.dbc'):
-        db.add_dbc_file('./dbc/'+file)
+def load_dbc():
+    global db
+    db = cantools.database.Database()
+    for file in os.listdir('./dbc'):
+        #print(file)
+        if file.endswith('.dbc'):
+            db.add_dbc_file('./dbc/'+file)
 # create a bus instance with python-can library
 # MUST HAVE can0 interface up and started
 # pi is set up to do this automatically on boot
@@ -25,17 +27,27 @@ for file in os.listdir('./dbc'):
 # up /sbin/ifconfig $IFACE up
 # down /sbin/ifconfig $IFACE down
 ###################
-bus = can.interface.Bus("can0", bustype='socketcan')
 
-while True:
-    try:
-        message = bus.recv()
-        # parse the name by using the frame_id and then pulling name attribute off that message
-        message_name = db.get_message_by_frame_id(message.arbitration_id).name
-        if message_name == "m1_Temperature":
-            # find message by string name MUT 1 temperature
-            temp_data = db.decode_message(message.arbitration_id,message.data)
-            print(temp_data["tm_m1_Inverter"])
-    except KeyError:
-        # key error exception will result if arb_id is not on the canbus 
-        pass
+def createSocket(enabled=False):
+    global bus
+    if enabled:
+        bus = can.interface.Bus("can0", bustype='socketcan')
+    else:
+        bus.shutdown()
+def readInverterTemp():
+    while True:
+        try:
+            message = bus.recv()
+            # parse the name by using the frame_id and then pulling name attribute off that message
+            message_name = db.get_message_by_frame_id(message.arbitration_id).name
+            if message_name == "m1_Temperature":
+                # find message by string name MUT 1 temperature
+                temp_data = db.decode_message(message.arbitration_id,message.data)
+                print(temp_data["tm_m1_Inverter"])
+        except KeyError:
+            # key error exception will result if arb_id is not on the canbus 
+            pass
+
+load_dbc()
+createSocket(True)
+readInverterTemp()
